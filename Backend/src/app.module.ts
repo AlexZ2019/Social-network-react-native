3a40d7e6b3e41670fc0ca3e22ca31d8947810f1f
Alex <alex@konstankino.com>
2023-01-31 13:12:41 +0200

    formatting

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,12 +1,12 @@
-import {Module} from '@nestjs/common';
-import {AppResolver} from './app.resolver';
-import {GraphQLModule} from '@nestjs/graphql';
-import {ApolloDriver, ApolloDriverConfig} from '@nestjs/apollo';
-import {ConfigModule} from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { AppResolver } from './app.resolver';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true, envFilePath: './.env'}),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: './.env' }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
