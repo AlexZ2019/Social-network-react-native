d4f97b267e61de567904e6b32a8ee0ce2f40cb3f
Alex <alex@konstankino.com>
2023-01-31 12:09:34 +0200

    removed idea files

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      sortSchema: true,
+    }),
+  ],
+  controllers: [],
+  providers: [AppResolver],
 })
 export class AppModule {}
