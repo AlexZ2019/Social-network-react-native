20c24a8a441b1bf38ddcdd0c7eb71be13bb3870a
Alex <alex@konstankino.com>
2023-02-06 09:27:12 +0200

    Added async storage methods

--- Frontend/src/utils/asyncStorage/index.ts
+++ Frontend/src/utils/asyncStorage/index.ts
@@ -1,6 +1,18 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+export const setAsyncStorageValue = async (key: string, value: string) => {
+  await AsyncStorage.setItem.setItem(key, value);
+};
+
+export const getAsyncStorageValue = (key: string) => {
+  return AsyncStorage.getItem(key);
+};
+
+export const removeAsyncStorageValue = (key: string) => {
+  return AsyncStorage.removeItem(key);
+};
+
 export const setTokensToAsyncStorage = async (tokens: { accessToken: string; refreshToken: string }) => {
-  await AsyncStorage.setItem('accessToken', tokens.accessToken);
-  await AsyncStorage.setItem('refreshToken', tokens.refreshToken);
+  await setAsyncStorageValue.setItem('accessToken', tokens.accessToken);
+  await setAsyncStorageValue.setItem('refreshToken', tokens.refreshToken);
 };
