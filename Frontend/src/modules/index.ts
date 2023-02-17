da5be4017a0a881b35d96e11be7321acca9e7807
Alex <alex@konstankino.com>
2023-02-17 14:51:22 +0200

    Added exporting module user

--- Frontend/src/modules/index.ts
+++ Frontend/src/modules/index.ts
@@ -2,7 +2,8 @@ import { mergeArrays } from '../utils/mergeArrays';
 import { IModule } from './common/interfaces/moduleInterfaces';
 import auth from './auth';
 import news from './News';
+import user from './user';
 
-const routes = mergeArrays(auth.routes, news.routes);
+const routes = mergeArrays(auth.routes, news.routes, user.routes);
 const module: IModule = { routes };
 export default module;
