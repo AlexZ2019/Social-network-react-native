1d6d380066c54f08ae4168c1935cc1f68d1955b3
Alex <alex@konstankino.com>
2023-05-31 08:44:16 +0300

    Added modal for deleting post

--- Frontend/src/modules/post/components/DeletePost/index.tsx
+++ Frontend/src/modules/post/components/DeletePost/index.tsx
@@ -1,27 +1,40 @@
-import { ActivityIndicator, Button, Icon } from '@ant-design/react-native';
+import {
+  ActivityIndicator,
+  Icon, Modal,
+} from '@ant-design/react-native';
 import { useMutation } from '@apollo/client';
 import { DELETE_POST } from '../../graphql/mutations/deletePost';
+import { View } from 'react-native';
+import React from 'react';
 
 const DeletePost = ({ id }: { id: number }) => {
-  
   const [deletePost, { loading }] = useMutation(DELETE_POST);
   
-  const onPressHandler = async () => {
-    await deletePost({
-      variables: { id },
-      update(cache) {
-        const normalizedId = cache.identify({ id, __typename: 'PostModel' });
-        cache.evict({ id: normalizedId });
-        cache.gc();
+  const onButtonClick = () => {
+    Modal.alert('Delete Post?', '', [
+      { text: 'Cancel', onPress: () => {}, style: 'cancel' },
+      {
+        text: 'OK', onPress: async () => await deletePost({
+          variables: { id },
+          update(cache) {
+            const normalizedId = cache.identify(
+              { id, __typename: 'PostModel' });
+            cache.evict({ id: normalizedId });
+            cache.gc();
+          },
+        }),
       },
-    });
+    ]);
   };
   
   if (loading) {
     return <ActivityIndicator/>;
   }
-  return <Icon onPress={onPressHandler} name="close-circle" size="md"
-               color="red"/>;
+  return (<View>
+      <Icon onPress={onButtonClick} name="close-circle" size="md"
+            color="red"/>
+    </View>
+  );
 };
 
 export default DeletePost;
