6ce8d1fe641300c47b22993b9e016205d8b9de2a
Alex <alex@konstankino.com>
2023-05-31 09:09:15 +0300

    Refactored GET_COMMENTS query

--- Frontend/src/modules/comment/graphql/queries/getComments.ts
+++ Frontend/src/modules/comment/graphql/queries/getComments.ts
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 export const GET_COMMENTS = gql`
-    query getComments($postId: Float!, $part: Float, $size: Float) {
-        getComments(postId: $postId, part: $part, size: $size) {
+    query getComments($postId: Float!, $part: Float) {
+        getComments(postId: $postId, part: $part) {
             total
             parts
             comments {
