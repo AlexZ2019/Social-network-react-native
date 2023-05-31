05d7c59081fdafc1f762558a1c60234d06445df9
Alex <alex@konstankino.com>
2023-05-31 08:45:12 +0300

    Added new fields for get post query

--- Frontend/src/modules/post/graphql/queries/getPosts.ts
+++ Frontend/src/modules/post/graphql/queries/getPosts.ts
@@ -1,11 +1,15 @@
 import gql from 'graphql-tag';
 
 export const GET_POSTS = gql`
-    query GetPosts($userId: Float) {
-        getUserPosts(userId: $userId) {
-            text
-            id
-            media
+    query GetPosts($userId: Float, $page: Float) {
+        getUserPosts(userId: $userId, page: $page) {
+            total
+            pages
+            posts {
+                id
+                media
+                text
+            }
         }
     }
 `;
