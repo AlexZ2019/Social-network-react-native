6e6e389d98e25beb0afcf6fcdf10a9966984a9b4
Alex <alex@konstankino.com>
2023-05-31 08:58:48 +0300

    Added getting more posts

--- Frontend/src/modules/post/components/Posts/index.tsx
+++ Frontend/src/modules/post/components/Posts/index.tsx
@@ -1,16 +1,18 @@
-import { useQuery } from '@apollo/client';
+import { NetworkStatus, useQuery } from '@apollo/client';
 import { GET_POSTS } from '../../graphql/queries/getPosts';
 import { FlatList } from 'react-native';
 import Post from '../Post';
 import { ActivityIndicator } from '@ant-design/react-native';
 import * as React from 'react';
+import { useState } from 'react';
 
 type Props = {
   userId: number | undefined
 }
 
 const Posts = ({ userId }: Props) => {
-  const { data, loading } = useQuery(GET_POSTS, {
+  const [page, setPage] = useState<number>(1);
+  const { data, loading, fetchMore, networkStatus } = useQuery(GET_POSTS, {
     variables: {
       userId,
     },
@@ -20,14 +22,27 @@ const Posts = ({ userId }: Props) => {
     return <ActivityIndicator/>;
   }
   
+  const getMorePosts = async () => {
+    if (page < data?.getUserPosts.pages) {
+      setPage(prevState => prevState + 1);
+      await fetchMore({ variables: { page: page + 1, userId } });
+    }
+  };
+  
   return (
-    <FlatList
-      data={data.getUserPosts}
-      renderItem={({ item }: any) => {
-        return <Post text={item.text} media={item.media}
-                     key={item.id} id={item.id} isEditable={!userId}/>;
-      }}
-    />
+    <>
+      <FlatList
+        data={data?.getUserPosts.posts}
+        onEndReached={getMorePosts}
+        onEndReachedThreshold={0.25}
+        renderItem={({ item }: any) => {
+          return <Post text={item.text} media={item.media}
+                       key={item.id} id={item.id} isEditable={!userId}/>;
+        }}
+      />
+      <ActivityIndicator animating={networkStatus === NetworkStatus.fetchMore
+        || loading}/>
+    </>
   );
 };
 
