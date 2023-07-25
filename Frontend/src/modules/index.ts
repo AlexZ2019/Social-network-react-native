e12907e652bc8e9c9200999566872f9bd057c989
Alex <alex@konstankino.com>
2023-07-25 11:59:05 +0300

    Added setting module on frontend

--- Frontend/src/modules/index.ts
+++ Frontend/src/modules/index.ts
@@ -4,8 +4,9 @@ import auth from './auth';
 import news from './News';
 import user from './user';
 import friend from './friend';
+import setting from './setting';
 
 const routes = mergeArrays(auth.routes, news.routes, friend.routes,
-  user.routes);
+  user.routes, setting.routes);
 const module: IModule = { routes };
 export default module;
