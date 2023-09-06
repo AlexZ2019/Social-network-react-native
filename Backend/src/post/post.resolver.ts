95d8b5ddbeefa9f8616e7887f490959f7a1208ef
Alex <alex@konstankino.com>
2023-09-06 09:10:54 +0300

    Added error handling for post module

--- Backend/src/post/post.resolver.ts
+++ Backend/src/post/post.resolver.ts
@@ -1,6 +1,6 @@
 import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import PostService from './post.service';
-import { Injectable, UseGuards } from '@nestjs/common';
+import { BadRequestException, Injectable, UseGuards } from '@nestjs/common';
 import AccessTokenGuard from '../auth/guards/accessToken.guard';
 import PostArgs from './dto/post.dto';
 import EditPostArgs from './dto/editPost.dto';
@@ -17,45 +17,61 @@ class PostResolver {
     private readonly postService: PostService,
     private readonly userService: UserService,
   ) {}
-  
+
   @Query(() => PostsModel)
   async getUserPosts(
     @Context() context,
     @Args() args: GetPostsDto,
   ): Promise<PostsModel> {
-    return this.postService.getUserPosts(
-      args?.userId || context.req.user.id,
-      args.page,
-      args.pageSize,
-    );
+    try {
+      return this.postService.getUserPosts(
+        args?.userId || context.req.user.id,
+        args.page,
+        args.pageSize,
+      );
+    } catch (e) {
+      throw new BadRequestException('Error getting posts');
+    }
   }
   
   @Mutation(() => Boolean)
   async createPost(@Args() post: PostArgs, @Context() context) {
-    const user = await this.userService.getUserById(context.req.user.id);
-    await this.postService.createPost({
-      ...post,
-      userId: context.req.user.id,
-      name:
-        user.firstname && user.lastname
-          ? `${user.firstname} ${user.lastname}`
-          : null,
-      nickname: user.nickname || null,
-      email: user.email,
-    });
-    return true;
+    try {
+      const user = await this.userService.getUserById(context.req.user.id);
+      await this.postService.createPost({
+        ...post,
+        userId: context.req.user.id,
+        name:
+          user.firstname && user.lastname
+            ? `${user.firstname} ${user.lastname}`
+            : null,
+        nickname: user.nickname || null,
+        email: user.email,
+      });
+      return true;
+    } catch ({ message }) {
+      throw new BadRequestException(message);
+    }
   }
   
   @Mutation(() => Boolean)
   async editPost(@Args() post: EditPostArgs, @Context() context) {
-    await this.postService.editPost(context.req.user.id, post);
-    return true;
+    try {
+      await this.postService.editPost(context.req.user.id, post);
+      return true;
+    } catch ({ message }) {
+      throw new BadRequestException(message);
+    }
   }
   
   @Mutation(() => Boolean)
   async deletePost(@Args() args: DeleteArgs, @Context() context) {
-    await this.postService.deletePost(args.id, context.req.user.id);
-    return true;
+    try {
+      await this.postService.deletePost(args.id, context.req.user.id);
+      return true;
+    } catch ({ message }) {
+      throw new BadRequestException(message);
+    }
   }
 }
 
