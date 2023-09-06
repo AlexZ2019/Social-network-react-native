3c97de162a58b8f13e93286ca3baa046768cee29
Alex <alex@konstankino.com>
2023-09-06 09:03:44 +0300

    Added error handling for news module

--- Backend/src/news/news.resolver.ts
+++ Backend/src/news/news.resolver.ts
@@ -1,6 +1,6 @@
 import { Args, Context, Query, Resolver } from '@nestjs/graphql';
 import NewsService from './news.service';
-import { Injectable, UseGuards } from '@nestjs/common';
+import { BadRequestException, Injectable, UseGuards } from '@nestjs/common';
 import AccessTokenGuard from '../auth/guards/accessToken.guard';
 import PostsModel from '../post/model/posts.model';
 import GetNewsDto from './dto/news.dto';
@@ -10,17 +10,21 @@ import GetNewsDto from './dto/news.dto';
 @UseGuards(AccessTokenGuard)
 class NewsResolver {
   constructor(private readonly newsService: NewsService) {}
-  
+
   @Query(() => PostsModel)
   async getNews(
     @Context() context,
     @Args() args: GetNewsDto,
   ): Promise<PostsModel> {
-    return this.newsService.getNews(
-      context.req.user.id,
-      args.page,
-      args.pageSize,
-    );
+    try {
+      return this.newsService.getNews(
+        context.req.user.id,
+        args.page,
+        args.pageSize,
+      );
+    } catch (e) {
+      throw new BadRequestException('Error getting news');
+    }
   }
 }
 
