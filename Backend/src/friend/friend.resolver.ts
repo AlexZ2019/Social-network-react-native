c836e147d32d474d88ad8bfe1b6a5c9e54928424
Alex <alex@konstankino.com>
2023-09-06 09:02:50 +0300

    Added error handling for friend module

--- Backend/src/friend/friend.resolver.ts
+++ Backend/src/friend/friend.resolver.ts
@@ -1,9 +1,6 @@
 import { Args, Context, Query, Resolver } from '@nestjs/graphql';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import FriendService from './friend.service';
-import { Injectable, UseGuards } from '@nestjs/common';
-import Token from '../auth/entities/token.entity';
+import { BadRequestException, Injectable, UseGuards } from '@nestjs/common';
 import UsersArgs from '../user/dto/users.dto';
 import UsersModel from '../user/model/users.model';
 import AccessTokenGuard from '../auth/guards/accessToken.guard';
@@ -16,7 +13,14 @@ class FriendResolver {
   @Query(() => UsersModel)
   @UseGuards(AccessTokenGuard)
   async getFriends(@Args() args: UsersArgs, @Context() context) {
-    return this.friendService.getFriends(context.req.user.id, args.searchValue);
+    try {
+      return this.friendService.getFriends(
+        context.req.user.id,
+        args.searchValue,
+      );
+    } catch (e) {
+      throw new BadRequestException('Error getting friends');
+    }
   }
 }
 
